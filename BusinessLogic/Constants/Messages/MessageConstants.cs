21f04b8d5c0c39d1388379b41c48202b5087a580
Dat Nguyen <nvdat0204@gmail.com>
2024-03-19 21:42:47 +0700

    Update Message

--- BusinessLogic/Constants/Messages/MessageConstants.cs
+++ BusinessLogic/Constants/Messages/MessageConstants.cs
@@ -2,16 +2,22 @@
 {
     public static class MessageConstants
     {
-        public const string NotFound = "Item not found.";
+        public const string NotFound = "Dữ liệu không tìm thấy.";
         public const string UpdateSuccess = "Chỉnh sửa thành công.";
-        public const string UpdateFail = "Updated Fail.";
+        public const string UpdateFail = "Chỉnh sửa không thành công.";
         public const string AddSuccess = "Thêm mới thành công.";
-        public const string AddFail = "Add Fail.";
+        public const string AddFail = "Thêm mới không thành công.";
         public const string DeleteSuccess = "Xoá thành công.";
+        public const string DeleteFail = "Xoá không thành công.";
         public const string IsInvalidEmail = "Email không hợp lệ.";
-        public const string CheckEmailExists = "Email đã tồn tại.";
         public const string PasswordIsInCorrect = "Mật khẩu hiện tại không đúng.";
         public const string PasswordNotMatch = "Mật khẩu không trùng khớp.";
         public const string ChangePasswordWasFail = "Thay đổi mật khẩu không thành công.";
+        public const string ImportSuccess = "Import dữ liệu thành công.";
+        public const string ImportFail = "Import dữ liệu không thành công.";
+        public const string CheckExists = "{0} đã tồn tại.";
+        public const string DoesNotExists = "{0} không tồn tại.";
+        public const string CheckDuplicate = "{0} trùng với {1}.";
+        public const string RequireField = "Vui lòng nhập trường này.";
     }
 }
