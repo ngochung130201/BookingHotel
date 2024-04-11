790ac385e8d4e308f04cc30e8b93be29195ad8b5
DatNguyen1091 <nvdat0204@gmail.com>
2024-04-11 17:08:03 +0700

    update

---
+++ WebApp/wwwroot/client-app/controllers/roomDetail/index.js
@@ -0,0 +1,130 @@
+﻿
+var NewsController = function () {
+    this.initialize = function () {
+        loadData();
+        registerEvents();
+        registerControls();
+    }
+
+    var registerEvents = function () {
+        //Init validation
+        $('#formMaintainance').validate({
+            errorClass: 'text-danger',
+            ignore: [],
+            lang: 'en',
+            rules: {
+                txtContent: {
+                    required: true
+                }
+            }
+        });
+
+        // Event search 
+        $('#txtKeyword').on('focusout', function (e) {
+            e.preventDefault();
+            loadData(true);
+        });
+        $('#txtKeyword').on('keypress', function (e) {
+            if (e.which === 13) {
+                e.preventDefault();
+                loadData(true);
+            }
+        });
+
+        // Event select page size
+        $("#ddl-show-page").on('change', function () {
+            base.configs.pageSize = $(this).val();
+            base.configs.pageIndex = 1;
+            loadData(true);
+        });
+
+        // Event save
+        $('#btnSave').on('click', function (e) {
+            e.preventDefault();
+            saveData(false);
+        });
+
+        // Event save
+        $('.btn-postComment').on('click', function (e) {
+            e.preventDefault();
+            saveData(false);
+        });
+
+        // Event button delete
+        $('body').on('click', '.btn-delete', function (e) {
+            e.preventDefault();
+            var id = $(this).data('id');
+            base.confirm('Bạn có chắc chắn muốn xóa?', function () {
+                deteleItem(id);
+            });
+        });
+    }
+
+    var deteleItem = function (id) {
+        $.ajax({
+            type: "POST",
+            url: "/Room/Delete",
+            data: {
+                id: id
+            },
+            dataType: "json",
+            beforeSend: function () {
+                base.startLoading();
+            },
+            success: function (response) {
+                if (response.succeeded) {
+                    base.notify(response.messages[0], 'success');
+                    base.stopLoading();
+                    loadData(true);
+                } else {
+                    base.notify(response.messages[0], 'error');
+                    base.stopLoading();
+                }
+            },
+            error: function (status) {
+                base.notify('Đang xảy ra lỗi!', 'error');
+                base.stopLoading();
+            }
+        });
+    }
+
+    var resetFormMaintainance = function () {
+        $('#hidId').val(0);
+        $('#txtContent').val('');
+    }
+    var saveData = function () {
+        if ($('#formMaintainance').valid()) {
+            var id = $('#hidId').val();
+            var roomId = $('#RoomId').val();
+            var content = $('#txtContent').val();
+            $.ajax({
+                type: "POST",
+                url: "/Room/SaveEntity",
+                data: {
+                    Id: id,
+                    Content: content,
+                    RoomId: roomId,
+                },
+                dataType: "json",
+                beforeSend: function () {
+                    base.startLoading();
+                },
+                success: function (response) {
+                    if (response.succeeded) {
+                        base.notify(response.messages[0], 'success');
+                        resetFormMaintainance();
+                        base.stopLoading();
+                        loadData(true);
+                    } else {
+                        base.notify(response.messages[0], 'error');
+                    }
+                },
+                error: function () {
+                    base.notify('Đang xảy ra lỗi!', 'error');
+                    base.stopLoading();
+                }
+            });
+            return false;
+        }
+    }
+}
